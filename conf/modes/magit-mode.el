7f0d79cc8c8b40700b1a75ceefbb92310d7f059f
Daniel Russo <danrusso@gmail.com>
2021-01-23 11:09:01 -0500

    Tweak magit configuration.

--- conf/modes/magit-mode.el
+++ conf/modes/magit-mode.el
@@ -1,3 +1,5 @@
+(require 'magit)
+
 ;; Display magit buffers in the current window, rather than a new window.
 (setq magit-display-buffer-function
       (lambda (buffer)
@@ -5,4 +7,10 @@
 
 ;; Don't show the diff buffer when authoring a commit. To manually bring up the
 ;; diff, use C-c C-d.
+;;
+;; The `server-switch-hook' change is documented here:
+;;
+;;   <https://magit.vc/manual/magit/Performance.html>
+;;
+(setq magit-commit-show-diff nil)
 (remove-hook 'server-switch-hook 'magit-commit-diff)
