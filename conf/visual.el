8df28ea522a48ba7871597ff1160021b19dad19f
Daniel Russo <danrusso@gmail.com>
2021-05-20 09:35:11 -0400

    Customize the theme and faces.

--- conf/visual.el
+++ conf/visual.el
@@ -1,7 +1,36 @@
 ;; Set the font size to 10pt. Note for presentations, 16pt is a good size.
 (set-face-attribute 'default nil :height 100)
 
-(load-theme 'molokai t)
+(load-theme 'badger t)
+
+;; Customize the colours.
+(set-face-attribute 'default nil
+                    :background "#0D0D12"
+                    )
+(set-face-attribute 'region nil
+                    :background "#4371A1"
+                    )
+(set-face-attribute 'fringe nil
+                    :background "#15151F"
+                    )
+(set-face-attribute 'window-divider nil
+                    :foreground "#363338"
+                    )
+(set-face-attribute 'font-lock-doc-face nil
+                    :foreground "#E6DB74"
+                    )
+(set-face-attribute 'font-lock-string-face nil
+                    :foreground "#E6DB74"
+                    )
+(set-face-attribute 'font-lock-constant-face nil
+                    :foreground "#86B187"
+                    )
+(set-face-attribute 'font-lock-preprocessor-face nil
+                    :foreground "wheat"
+                    )
+(set-face-attribute 'font-lock-type-face nil
+                    :foreground "#D4AED4"
+                    )
 
 ;; Blink the cursor.
 (blink-cursor-mode t)
@@ -11,7 +40,11 @@
 
 ;; Turn on line numbers.
 (global-display-line-numbers-mode)
-(set-face-attribute 'line-number-current-line nil :foreground "#66D9EF")
+(set-face-attribute 'line-number-current-line nil
+                    :weight 'bold
+                    :foreground "#F28B86"
+                    :background "#0D0D15"
+                    )
 
 ;; Add and customize a divider between windows.
 (setq window-divider-default-right-width 1)
@@ -27,5 +60,3 @@
 (setq highlight-indent-guides-method 'character)
 (setq highlight-indent-guides-responsive 'stack)
 (setq highlight-indent-guides-auto-character-face-perc 4)
-
-
