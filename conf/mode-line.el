7bbbbe1e0e07b0c4b72298aca323e9b127c0bd8d
Daniel Russo <danrusso@gmail.com>
2021-05-23 11:15:47 -0400

    Update the mode line.

--- conf/mode-line.el
+++ conf/mode-line.el
@@ -1,13 +1,15 @@
-;; Override the `mode-line' face, making it taller.
+;; Override the `mode-line' face.
 (set-face-attribute 'mode-line nil
-                    :background "#394952"
-                    :box '(:line-width 8 :color "#394952"))
+                    :foreground "#656868"
+                    :background "#23334a"
+                    :box '(:line-width 5 :color "#23334a"))
 
 ;; Override the `mode-line-inactive' face. It has a darker colour than the
-;; active face, and it is not quite as tall.
+;; active face.
 (set-face-attribute 'mode-line-inactive nil
-                    :background "#2e363b"
-                    :box '(:line-width 5 :color "#2e363b"))
+                    :foreground "#656868"
+                    :background "#1c212e"
+                    :box '(:line-width 5 :color "#1c212e"))
 
 (defface dr/mode-line-file-name-face
   '((t
@@ -31,7 +33,7 @@ isn't visiting a file)."
 (defface dr/mode-line-project-face
   '((t
      :weight normal
-     :foreground "#8db6cd" ;; THis colour is "SkyBlue3".
+     :foreground "#8db6cd" ;; This colour is "SkyBlue3".
      :background nil
      :box nil))
   "Face for the file's project."
@@ -66,31 +68,40 @@ isn't visiting a file)."
 
 ;; An `alist' cache of values that are (relatively) expensive to compute on
 ;; every mode line redraw.
-(setq *dr/mode-line-cache* nil)
+(setq dr/mode-line-cache nil)
 
 (defun dr/mode-line-populate-cache ()
   "Populate all of the values (buffer local) we keep in the cache
 for each mode line redraw."
-  (setq-local *dr/mode-line-cache*
+  (setq-local dr/mode-line-cache
               `((project-name . ,(projectile-project-name))
-                (project-root . ,(projectile-project-root)))))
+                (project-root . ,(projectile-project-root))
+                (buffer-file-name . ,(buffer-file-name))
+                (file-name-nondirectory . ,(let ((name (buffer-file-name)))
+                                             (if name (file-name-nondirectory name) nil)))
+                (file-path-project-relative . ,(let ((name (buffer-file-name))
+                                                     (project (projectile-project-name)))
+                                                 (if name
+                                                     (let ((dir (file-name-directory name)))
+                                                       (if project
+                                                           (file-relative-name dir (projectile-project-root))
+                                                         dir))
+                                                   ""))))))
 
 (defun dr/mode-line-get (key)
   "Get a value from the mode line values cache. If the cache
-isn't yet populated, populates it first."
-  (if *dr/mode-line-cache*
+isn't yet populated, populate it first."
+  (if dr/mode-line-cache
       nil
       (dr/mode-line-populate-cache))
-  (cdr (assoc key *dr/mode-line-cache*)))
+  (cdr (assoc key dr/mode-line-cache)))
 
 (defun dr/mode-line-file-name-or-buffer-name ()
   "For use in `mode-line-format'. Returns the file name if the
 current buffer is visiting a file, otherwise returns the '%b'
 construct (which will print the buffer name as-is)."
-  (let ((name (buffer-file-name)))
-    (if name
-        (file-name-nondirectory name)
-      "%b")))
+  (let ((name (dr/mode-line-get 'file-name-nondirectory)))
+    (or name "%b")))
 
 (defun dr/mode-line-project ()
   "For use in `mode-line-format'. Returns the projectile project
@@ -101,25 +112,17 @@ project. Otherwise, an empty string is returned."
 
 (defun dr/mode-line-file-dir ()
   "For use in `mode-line-format'. If the buffer is visiting a
-  file and the file is part of a projectile project, returns the
-  directory path relative to the project root (best used in
-  conjunction with `dr/mode-line-project'). If the buffer is
-  visiting a file and the the file is not part of a projectile
-  project, the directory path is returned. Returns an empty
-  string if the the file is not visiting a file."
-  (let ((name (buffer-file-name))
-        (project (dr/mode-line-get 'project-name)))
-    (if name
-        (let ((dir (file-name-directory name)))
-          (if project
-              (file-relative-name dir (dr/mode-line-get 'project-root))
-            dir))
-      "")))
+file and the file is part of a projectile project, returns the
+directory path relative to the project root (best used in
+conjunction with `dr/mode-line-project'). If the buffer is
+visiting a file and the the file is not part of a projectile
+project, the directory path is returned. Returns an empty string
+if the the file is not visiting a file."
+  (dr/mode-line-get 'file-path-project-relative))
 
-;; Set the (default) template for the mode line.
-(setq-default mode-line-format
+(setq-default dr/mode-line-format-left
               (list
-	       mode-line-front-space
+               mode-line-front-space
 
                ;; The default emacs mode line includes the following, but they
                ;; are omitted here:
@@ -132,21 +135,23 @@ project. Otherwise, an empty string is returned."
                ;; in the buffer (the percent above the top window), and the
                ;; overall size of the buffer, respectively.
 
-	       mode-line-modified
+               mode-line-modified
 
-	       " "
+               " "
 
                ;; The file (or buffer) name. The face is selected based on
                ;; whether the buffer is modified or not.
                '(:eval
                  (let ((name (dr/mode-line-file-name-or-buffer-name))
-	               (face
+	                   (face
                         (if (buffer-modified-p)
                             'dr/mode-line-file-name-modified-face
-                          'dr/mode-line-file-name-face)))
-	           (propertize name
+                          'dr/mode-line-file-name-face))
+                       ;; (face 'dr/mode-line-file-name-face)
+                       )
+                   (propertize name
                                'face face
-                               'help-echo (buffer-file-name))))
+                               'help-echo (buffer-name))))
 
                " "
 
@@ -160,27 +165,46 @@ project. Otherwise, an empty string is returned."
                              'face 'dr/mode-line-file-dir-face))
 
                " "
+
                ;; The line and column counts (%c starts the column count at 0,
                ;; and %C at 1).
                (propertize "-%l '%C" 'face 'dr/mode-line-cursor-position-face)
+
                " "
+
                ;; The version control branch.
                '(:eval
                  (propertize (if vc-mode (substring vc-mode 5) "")
                              'face 'dr/mode-line-vc-branch-face
                              'help-echo nil))
-
-
-               ;; Add spaces to right align the major mode.
-               '(:eval
-                 (propertize " "
-                             'display `((space
-                                         :align-to
-                                         (- (+ right right-fringe right-margin)
-                                            ,(+ (+ 3 (string-width mode-name))))))))
-
-               ;; The major mode.
-               (propertize " %m "
-                           'face 'font-lock-string-face
-                           'help-echo nil)
                ))
+
+(setq-default dr/mode-line-format-right
+              (list
+               ;; The major mode.
+               '(:eval (propertize " %m "
+                                   'face 'font-lock-string-face
+                                   'help-echo nil))
+
+               " "
+
+               mode-line-misc-info
+
+               ))
+
+(defun dr/render-mode-line (left-format right-format)
+  (let* ((left (format-mode-line left-format))
+         (right (format-mode-line right-format))
+         (space (propertize
+                 " "
+                 'display `((space
+                             :align-to
+                             (- (+ right right-fringe right-margin)
+                                ,(+ (+ 3 (string-width right)))))))))
+    (format " %s%s%s " left space right)))
+
+;; Set the (default) template for the mode line.
+(setq-default mode-line-format
+              '((:eval (dr/render-mode-line
+                        dr/mode-line-format-left
+                        dr/mode-line-format-right))))
